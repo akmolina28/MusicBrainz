c66a1625e591b22ca25fe26a5aab32d97b22a29d
akmolina28 <akmolina28@gmail.com>
2016-07-07 11:36:00 -0400

    Added Relation entity and RelationList collection with support for Artist Relations (inc=artist-rels)

---
+++ Hqub.MusicBrainz/Hqub.MusicBrainz.API/Entities/Relation.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Hqub.MusicBrainz.API.Entities
+{
+    [XmlRoot("relation", Namespace = "http://musicbrainz.org/ns/mmd-2.0#")]
+    public class Relation
+    {
+        /// <summary>
+        /// MusicBrainz id of the type of the related entity (artist, area, etc.).
+        /// </summary>
+        [XmlAttribute("type-id")]
+        public string TypeId { get; set; }
+
+        /// <summary>
+        /// The type of related the entity.
+        /// </summary>
+        [XmlAttribute("type")]
+        public string Type { get; set; }
+
+        /// <summary>
+        /// Music Brainz id of the related entity.
+        /// </summary>
+        [XmlElement("target")]
+        public string Target { get; set; }
+
+        /// <summary>
+        /// Direction of the relationship (forward or backward).
+        /// </summary>
+        [XmlElement("direction")]
+        public string Direction { get; set; }
+
+        /// <summary>
+        /// The begin date of the relationship.
+        /// </summary>
+        [XmlElement("begin")]
+        public string Begin { get; set; }
+
+        /// <summary>
+        /// The end date of the relationship.
+        /// </summary>
+        [XmlElement("end")]
+        public string End { get; set; }
+
+        /// <summary>
+        /// "true" if relationship has ended.
+        /// </summary>
+        [XmlElement("ended")]
+        public string Ended { get; set; }
+
+        /// <summary>
+        /// The entity (if this instance is an artist relation).
+        /// </summary>
+        [XmlElement("artist")]
+        public Artist Artist { get; set; }
+    }
+}
